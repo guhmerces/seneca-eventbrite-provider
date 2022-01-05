f79e07201a739988c445c2ce02468e469b4594a0
Gustavo <guhmerces@hotmail.com>
2022-01-05 07:21:04 -0300

    Create type MediaUploadType

--- src/types.ts
+++ src/types.ts
@@ -7,7 +7,16 @@ type InitalCommandsArgs = {
   ZONE_BASE: string
 }
 
+type MediaUploadType =
+  | 'image-event-logo'
+  | 'image-event-logo-preserve-quality'
+  | 'image-event-view-from-seat'
+  | 'image-organizer-logo'
+  | 'image-user-photo'
+  | 'image-structured-content'
+
 export type {
   Eventbrite,
   InitalCommandsArgs,
+  MediaUploadType
 }
