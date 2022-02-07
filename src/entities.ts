6714dd0f26a71c8e6587baa76af8200b82b0b315
Gustavo <guhmerces@hotmail.com>
2022-02-07 11:21:55 -0300

    add save action to events ent in entities.ts

--- src/entities.ts
+++ src/entities.ts
@@ -11,6 +11,44 @@ const entities: EntityMap = {
         before: [
           { on:'query', field: 'attribute', set: { query: 'event_id' } }
         ]
+      },
+      save: {
+        request: {
+          method: "post",
+          path: "/events/:event_id",
+          body_spec: {
+            event: {
+              name: {
+                html: 'string',
+              },
+              description: {
+                html: 'string',
+              },
+              start: {
+                timezone: 'string',
+                utc: 'string',
+              },
+              end: {
+                timezone: 'string',
+                utc: 'string',
+              },
+              currency: 'string',
+              online_event: 'string',
+              organizer_id: 'string',
+              listed: 'string',
+              shareable: 'string',
+              invite_only: 'string',
+              show_remaining: 'string',
+              password: 'string',
+              capacity: 'string',
+              is_reserved_seating: 'string',
+              is_series: 'string',
+              show_pick_a_seat: 'string',
+              show_seatmap_thumbnail: 'string',
+              show_colors_in_seatmap_thumbnail: 'string',
+            }
+          }
+        }
       }
     },    
   }
